442c607e06e182bacf8894ae12567458be7cc2ef
polina <polinanonever@mail.ru>
2025-04-25 18:01:21 +0000

    spaces2

--- server/application.py
+++ server/application.py
@@ -5,13 +5,13 @@ PORT = 8000
 
 class TestMe():
     def take_five(self):
-      return 5
+        return 5
     def port(self):
-      return PORT
+        return PORT
 
 if __name__ =='__main__':
     Handler = http.server.SimpleHTTPRequestHandler
 
     with socketserver.TCPServer(("", PORT), Handler) as http:
-      print("serving at port", PORT)
-      http.serve_forever()
+        print("serving at port", PORT)
+        http.serve_forever()
